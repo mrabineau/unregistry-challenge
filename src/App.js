faf377c39e4af9382453560ef733b54e783096d0
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-02-13 13:53:26 -0800

    check box working

--- src/App.js
+++ src/App.js
@@ -7,19 +7,31 @@ constructor(props){
   super(props);
   this.state = {
     domain: ['foodfighters.lol', 'selfdriving.cars', 'greendiamondsky.com'],
-    price: ['12.00', '16.00', '9.00']
+    price: ['12.00', '16.00', '9.00'],
+
 
    }
+    // this.isChecked = this.isChecked.bind(this)
   }
+
+  // isChecked (index) {
+
+  //     const check = this.state.domain[0].charAt(str.length-3);
+  //       check === 'lol' ? true : false
+  //   }
+
 
 
     render() {
       let _ = require('underscore')
       const domains = this.state.domain;
-          _.each(domains, function(k, index, domains){
-            console.log(k)
-              return <a className="d" key={k}></a>
-          })
+      const each = _.each(domains, function(d){})
+      console.log(each[0]);
+
+      const price = this.state.price;
+      const eachPrice = _.each(price, function(p){})
+      console.log(price[0])
+
       return (
        <div id="center">
           <table className="table">
@@ -36,34 +48,52 @@ constructor(props){
                 <th scope="row">1</th>
                     <td>
                       <fieldset className="form-group">
+                          { each[0].includes("lol") || each[0].includes("cars")
+                            ?
+                          <input type="checkbox" id="checkbox1" checked></input>
+                            :
                           <input type="checkbox" id="checkbox1"></input>
+
+                           }
+
                           <label for="checkbox1"></label>
                       </fieldset>
                   </td>
-                    <td></td>
-                    <td>$12.00</td>
+                    <td><a>{each[0]}</a></td>
+                    <td>${price[0]}</td>
               </tr>
           <tr>
               <th scope="row">2</th>
               <td>
                   <fieldset className="form-group">
+                    { each[1].includes("lol") || each[1].includes("cars")
+                      ?
+                      <input type="checkbox" id="checkbox2" checked></input>
+                      :
                       <input type="checkbox" id="checkbox2"></input>
+
+                    }
                       <label for="checkbox2"></label>
                   </fieldset>
               </td>
-              <td><a>selfdriving.cars</a></td>
-              <td>$16.00</td>
+              <td><a>{each[1]}</a></td>
+              <td>${price[1]}</td>
           </tr>
           <tr>
               <th scope="row">3</th>
               <td>
                   <fieldset className="form-group">
-                      <input type="checkbox" id="checkbox3"></input>
+                  { each[2].includes("lol") || each[2].includes("cars")
+                      ?
+                      <input type="checkbox" id="checkbox3" checked></input>
+                      :
+                      <input type="checkbox" id="checkbox3" ></input>
+                  }
                       <label for="checkbox3"></label>
                   </fieldset>
               </td>
-              <td><a>greendiamondsky.com</a></td>
-              <td>$16.00</td>
+              <td><a>{each[2]}</a></td>
+              <td>${price[2]}</td>
           </tr>
       </tbody>
     </table>
